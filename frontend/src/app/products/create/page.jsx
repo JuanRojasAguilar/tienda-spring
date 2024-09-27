5ae1e4c247a4999e7fc6289126c030a9ca22379f
juanrojasaguilar <yo217@hotmail.es>
2024-09-27 09:57:36 -0500

    feat: :construction: label and input styles

--- frontend/src/app/products/create/page.jsx
+++ frontend/src/app/products/create/page.jsx
@@ -13,16 +13,48 @@ const Styles = {
         justify-center
         gap-4
     `,
-    title: `
+  title: `
         text-white
         text-6xl
         font-bold
     `,
-    fieldset: `
+  fieldset: `
         w-1/5
         flex
         justify-center
-    `
+    `,
+  label: `
+        w-1/2
+        text-white
+        text-3xl
+
+    `,
+  input: `
+        w-1/2
+        rounded-lg
+    `,
+  button: `
+    mx-4
+    rounded-xl
+    text-white
+    text-xl
+    font-bold
+    w-[240px]
+    h-[60px]
+    transition
+    hover:cursor-pointer
+    hover:shadow-lg
+  `,
+  registerBtn: `
+      bg-green-700
+      hover:bg-green-600
+      hover:shadow-green-700
+  `,
+  resetBtn: `
+      bg-red-500
+      hover:bg-red-600
+      hover:shadow-red-700
+  `,
 };
 
 const CreateProduct = () => {
@@ -30,31 +62,55 @@ const CreateProduct = () => {
     <form className={Styles.form}>
       <h1 className={Styles.title}>Crear Producto Nuevo</h1>
 
-      <fieldset className={Styles.fieldset}>
-        <label htmlFor="name" className="w-1/2">Nombre:</label>
-        <input type="text" name="name" id="name" className="w-1/2"/>
-      </fieldset>
+      <>
+        <fieldset className={Styles.fieldset}>
+          <label htmlFor="name" className={Styles.label}>
+            Nombre:
+          </label>
+          <input type="text" name="name" id="name" className={Styles.input} />
+        </fieldset>
 
-      <fieldset className={Styles.fieldset}>
-        <label htmlFor="name">Precio:</label>
-        <input
-          type="number"
-          name="name"
-          id="name"
-          min={0}
-          step={0.01}
-          max={1_000_000_000}
-        />
-      </fieldset>
+        <fieldset className={Styles.fieldset}>
+          <label htmlFor="name" className={Styles.label}>
+            Precio:
+          </label>
+          <input
+            type="number"
+            name="name"
+            id="name"
+            min={0}
+            step={0.01}
+            max={1_000_000_000}
+            className={Styles.input}
+          />
+        </fieldset>
 
-      <fieldset className={Styles.fieldset}>
-        <label htmlFor="name">Stock:</label>
-        <input type="number" name="name" id="name" min={0} max={2_000_000_000} />
-      </fieldset>
+        <fieldset className={Styles.fieldset}>
+          <label htmlFor="name" className={Styles.label}>
+            Stock:
+          </label>
+          <input
+            className={Styles.input}
+            type="number"
+            name="name"
+            id="name"
+            min={0}
+            max={2_000_000_000}
+          />
+        </fieldset>
+      </>
 
       <div>
-        <input type="submit" value="Registrar producto" />
-        <input type="submit" value="Reiniciar" />
+        <input
+          className={`${Styles.button} ${Styles.registerBtn}`}
+          type="submit"
+          value="Registrar producto"
+        />
+        <input
+          className={`${Styles.button} ${Styles.resetBtn}`}
+          type="submit"
+          value="Reiniciar"
+        />
       </div>
     </form>
   );
